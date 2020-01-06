d36a186580cec8beaaab194f95b96ee99ca7b323
kmbarnett <kmbarnett94@gmail.com>
2020-01-05 19:39:04 -0700

    Fix bug where awnser could be undifined

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var displayAnwser = document.querySelector('.awnser-js');
 var possibleAwnsers = ['It is certain', 'It is decidedly so.', 'Without a doubt', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Don\'t count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
 function randomAwnser() {
-  i = Math.round(Math.random() * possibleAwnsers.length);
+  i = Math.floor(Math.random() * possibleAwnsers.length);
   return possibleAwnsers[i]
 }
 
